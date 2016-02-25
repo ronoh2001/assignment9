e15a99aad8948dd4c0986c4a8c3ad739e57a5ca6
ron oh <ronoh2001@gmail.com>
2016-02-24 21:11:00 -0800

    update

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ console.log(config)
 var DB = config.DB
 var PORT = config.PORT
 var models = require('./models')
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 
 app.get('/articles', function(req, res) {
   models.Articles.findAll().then(function (articles) {
@@ -16,7 +21,7 @@ app.get('/articles', function(req, res) {
 models.sequelize.sync({force:true}).then(function(x) {
   models.Articles.create({
     author:'Ron Oh',
-    blog:'Hello this is my blog.  I have a lot to catch up on, but this is a beginning.  Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    blog:'Hello this is my blog.  I have a lot to catch up on, but this is a good start.  Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
   })
 })
   app.listen(PORT, function() {
